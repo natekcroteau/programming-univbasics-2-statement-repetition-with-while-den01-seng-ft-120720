a9d26864160e8f3721b2265fa6b58c576e6f1a3e
Nate Croteau <natekcroteau@gmail.com>
2020-11-11 18:23:16 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,9 +1,10 @@
 # Write your code here
 count = 10 
+its_time = 0
+
 
 while count > 0 do 
+  break if count == its_time puts "Happy New Year!"
   puts "#{count}"
   count -= 1
-break if count == 0 
-  puts "Happy New Year!"
 end
